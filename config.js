be174a1225e311d849df1a82da4b15d45ca15b2f
xMizuki <62658861+xMizuki@users.noreply.github.com>
2020-08-28 13:16:12 +0200

    Create config.js

---
+++ config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  token: "TOKEN", // Your bot's token
+  prefix: "p!",
+  ownerID: "ID", // Your ID
+  pg_user: "USER", // Your PostgreSQL User
+  pg_pass: "PASSWORD", // Your PostgreSQL User's password
+  pg_db: "test_bot" // The name of the database that you want to name. Make sure to run /database/generate-db.js before starting the bot
+}
